ca2e3b15959eb93e7d2d753cfbd243bdfe57d4cf
Chansa CHISHIMBA <chishimb_c@qwasar.io>
2020-04-28 20:18:31 +0000

    Added the conditional terminator 0

---
+++ ex02/my_strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int my_strlen (char *a)
+{
+    
+    int len;
+    //loop until encounter the string terminator '\0'
+    for(len=0;a[len]!='\0';len++);
+    return len;
+    return 0;
+}
+
+/*
+int main()
+{
+    printf("Zambia has %d characters\n",my_strlen("Zambia"));
+    //printf("My is negative : %d  \n", my_is_negative(4));
+    return 0;
+}
+
+*/
