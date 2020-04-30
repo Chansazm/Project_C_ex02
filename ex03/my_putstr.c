f36f0fb03d3ee4245f010912965c1ecf2547155f
Chansa CHISHIMBA <chishimb_c@qwasar.io>
2020-04-30 21:14:00 +0000

    Added the index

---
+++ ex03/my_putstr.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void my_putstr(char* a) {
+    int index;
+    
+    
+    index = 0;
+    while
+    (a[index] != '\0')
+    {
+        write (1,&a[index],1);
+        index += 1;
+    }
+    
+
+  
+}
+
+
+
+
+
+/*
+
+int main(){
+
+//abc-->abc
+//int a = 0;
+//printf("The value of a is %d\n",a);
+return 0;
+
+}*/
