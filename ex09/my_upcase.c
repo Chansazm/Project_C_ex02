ce229586c954a015bdb0d1e2cda77e712c248a5b
Chansa CHISHIMBA <chishimb_c@qwasar.io>
2020-05-01 23:13:09 +0000

    Added the strlen function

--- ex09/my_upcase.c
+++ ex09/my_upcase.c
@@ -1,7 +1,16 @@
-char* my_upcase(char* a) {
 
-    if ('a' >= 'A')
-    return 'A';
 
-  
+char *my_upcase(char *param_1) {
+
+  size_t len = strlen(param_1);
+  size_t i;
+
+  for (i = 0; i < len; i++) {
+    if (param_1[i] >= 'a' && param_1[i] <= 'z') 
+    {
+      param_1[i] = param_1[i] - 'a' + 'A'; 
+      
+    }
+  }
+  return param_1; 
 }
